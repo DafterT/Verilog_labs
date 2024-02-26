709a72b8cb3893fce7e2ad62f00d65656eef2dd3
Dafter <dafterplay@mail.ru>
2024-02-27 02:37:54 +0300

    update db, add sdc, add qar

--- 4_Lab_MS_SV_2/db_lab_MS_SV2.sv
+++ 4_Lab_MS_SV_2/db_lab_MS_SV2.sv
@@ -6,20 +6,21 @@ module db_lab_MS_SV2 (
   bit       front_door = 0;
   bit       rear_door = 0;
   bit       window = 0;
-  bit       clk = 0;
   bit       reset = 0;
   bit       ENA = 0;
   bit [3:0] keypad = 0;
   bit       alarm_siren;
   bit       is_armed;
   bit       is_wait_delay;
-  
-  lab_MS_SV2 u_lab_MS_SV2 (.*);
 
-	SP_unit u_SP_unit (
-		.source     ({front_door, rear_door, window, reset, ENA, keypad}),
-		.source_clk (CLK)  
-	);
+  lab_MS_SV2 u_lab_MS_SV2 (
+    .*,
+    .clk(CLK)
+  );
 
-	
+  SP_unit u_SP_unit (
+    .source    ({front_door, rear_door, window, reset, ENA, keypad}),
+    .source_clk(CLK)
+  );
+
 endmodule
