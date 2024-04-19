7bf03a62697579c39f561b5c26beeec9645ef787
DafterPlay <dafterplay@mail.ru>
2024-04-19 16:34:25 +0300

    add gen module and tb

---
+++ curse_project/project_1/gen.sv
@@ -0,0 +1,32 @@
+module gen (
+  input  bit       clk,
+  input  bit       ENgen,
+  input  bit       ENwrk,
+  input  bit       full,
+  output bit       wrreq,
+  output bit [7:0] data
+);
+
+  bit       RST = 1'b0;
+  bit       ENA = 1'b0;
+  bit [7:1] LFSR_out = 1'b0;
+
+  LFSR_7_6_3_1_0_F u_LFSR_7_6_3_1_0_F (
+    .CLK(clk),
+    .RST,
+    .ENA,
+    .LFSR_out
+  );
+
+  assign data = {1'b0, LFSR_out};
+
+  always_ff @(posedge clk) begin
+    if (ENgen == 1'b1 && full == 0 && ENwrk == 1) begin
+      wrreq <= 1'b1;
+      ENA   <= 1'b1;
+    end else begin
+      wrreq <= 1'b0;
+      ENA   <= 1'b0;
+    end
+  end
+endmodule
---
+++ curse_project/project_1/gen_tb.sv
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ns
+module tb_gen;
+
+  parameter PERIOD = 10;
+
+  bit       clk = 1'b0;
+  bit       ENgen = 1'b0;
+  bit       ENwrk = 1'b0;
+  bit       full = 1'b0;
+  bit       wrreq;
+  bit [7:0] data;
+
+  initial begin
+    forever #(PERIOD / 2) clk = ~clk;
+  end
+
+  gen u_gen (
+    .clk,
+    .ENgen,
+    .ENwrk,
+    .full,
+    .wrreq,
+    .data
+  );
+
+  initial begin
+    #(PERIOD * 1);
+    ENgen = 1'b1;
+    #(PERIOD * 1);
+    ENwrk = 1'b1;
+    #(PERIOD * 10);
+    full = 1'b1;
+    #(PERIOD * 2);
+    full = 1'b0;
+    #(PERIOD * 4);
+    $stop;
+  end
+
+endmodule
