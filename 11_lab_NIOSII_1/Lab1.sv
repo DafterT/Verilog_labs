8a38c94484f40ba62b19532769f87f3a488da5ee
Dafter <dafterplay@mail.ru>
2024-04-21 15:02:57 +0300

    add report

--- 11_lab_NIOSII_1/Lab1.sv
+++ 11_lab_NIOSII_1/Lab1.sv
@@ -1,15 +1,15 @@
 module Lab1 (
-	input bit clk,
-	input bit [7:0] sw,
-	input bit pbb,
-	output bit [7:0] led
+  input  bit       clk,
+  input  bit [7:0] sw,
+  input  bit       pbb,
+  output bit [7:0] led
 );
 
-	Lab1_nios u0 (
-		.clk_clk			(clk),
-		.reset_reset_n	(pbb),
-		.led_export		(led),
-		.sw_export		(sw)
-	);
+  Lab1_nios u0 (
+    .clk_clk      (clk),
+    .reset_reset_n(pbb),
+    .led_export   (led),
+    .sw_export    (sw)
+  );
 
 endmodule
