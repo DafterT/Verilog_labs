bd0b1f5f9949a883c167dc8886352ed9debaba64
DafterPlay <dafterplay@mail.ru>
2024-05-03 10:38:52 +0300

    add project_2_lab_1

---
+++ curse_project/project_2/lab_1/software/lab3_sw/lab3_source.c
@@ -0,0 +1,34 @@
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
+#include <unistd.h>
+#include <stdio.h>
+#define NONE_PRESSED 0x1
+#define DEBOUNCE 50000
+
+int main (void) {
+	int buttons;
+	int led = 0x00;
+
+	printf("Привет\nПроцессор Nios II запущен!\n");
+	printf("Нажмите кнопку pba на плате miniDiLab-CIV\n\n");
+
+	IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE, led);
+
+	while (1) {
+		buttons = IORD_ALTERA_AVALON_PIO_DATA(BUTTONS_BASE);
+
+		if (buttons != NONE_PRESSED) {
+			printf("Нажата кнопка pba\n");
+
+			if (led >= 0x80 || led == 0x00) led = 0x01;
+			else led = led << 1;
+
+			IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE, ~led);
+
+			usleep(DEBOUNCE);
+			while(buttons != NONE_PRESSED)
+				buttons = IORD_ALTERA_AVALON_PIO_DATA(BUTTONS_BASE);
+			usleep(DEBOUNCE);
+		}
+	}
+}
